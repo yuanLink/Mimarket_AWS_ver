dedea4a5dc08d210cd9d8b2c92df9d06fe03d6cf
link-chen <Link_Chen@trendmicro.com>
2019-05-23 16:35:13 +0800

    Add Control module with testcase

--- Control/BuyHandle.py
+++ Control/BuyHandle.py
@@ -36,7 +36,7 @@ class BuyHandle(object):
         self.redisHandle.connect_redis()
         # [TODO] check return value
 
-    def insert_buy_information(self, info):
+    def insert_buy_information(self, info_array):
         """ insert buying information from website
         info: Basic Information for phone and address
 
@@ -45,39 +45,80 @@ class BuyHandle(object):
         queue is up to READY_FOR_REDIS, we will write to 
         reids
         """
-        if self.infoQueue.full():
-            multi_data = []
-            while not self.infoQueue.empty():
-                multi_data.append(self.infoQueue.get_nowait())
+        for each_info in info_array:
+            # convert to tuple version
+            self.infoQueue.put_nowait(each_info.get_data())
+            if self.infoQueue.full():
+                multi_data = []
+                while not self.infoQueue.empty():
+                    multi_data.append(self.infoQueue.get_nowait())
 
-            self.redisHandle.set_multiple_data(multi_data)
+                #  now the queue is empty
+                self.redisHandle.set_multiple_data(multi_data)
+        
+        # here we maybe last some data, we will call last_buy to finish it
+        print("[+] now we last {} element".format(self.infoQueue.qsize()))
 
-        self.infoQueue.add(info.get_data())
-
-    def fininsh_buy(self):
+    def last_buy(self):
         """ insert last of data in queue into the redis
         """
+        multi_data = []
         while not self.infoQueue.empty():
             multi_data.append(self.infoQueue.get_nowait())
         self.redisHandle.set_multiple_data(multi_data)
+
+
+    def check_data(self, phone_array):
+        """ check data in redis
+        phone_array the array of string saved phone
+
+        if success, it will return data which save in redis
+        """
+        return self.redisHandle.get_multiple_data(phone_array)
+
+    def finish_buy(self):
+        """ finish buying control
+        It will deal with some resource restore, and disclose redis
+        """
+        self.redisHandle.close_redis()
 
 
 
 def testcase_buy_handle():
 
     testcase = []
+    phonecase = []
+    checkcase = []
     for i in range(1000):
         phone = str(i).zfill(6)
         addr = str(i)
-        testcase.append((phone, addr))
+        info = BasicInfo(addr, phone)
+        testcase.append(info)
+        phonecase.append(phone)
+        checkcase.append(addr)
 
     buyHandle = BuyHandle()
     buyHandle.prepare_buy()
 
+    # saving data in redis
     buyHandle.insert_buy_information(testcase)
-    buyingHandle.fininsh_buy()
+    # and we finish buying
+    buyHandle.last_buy()
 
+    # then we get data from redis to checkout it could save it
+    pass_ = True
+    tmp_case = buyHandle.check_data(phonecase)
+    for check, tmp in zip(checkcase, tmp_case):
+        if check != tmp.decode("utf-8"):
+            print("[*] Error in get data testing:{}:{}".format(check, tmp))
+            pass_ = False
+
+    if pass_:
+        print("[=] Test passed")
+    # finally,end of this handle
+    buyHandle.finish_buy()
+    
 
 
 if __name__ == '__main__':
-    buy = BuyHandle()
+    testcase_buy_handle()
