d493a622227d9b1719c25b5de3dfa73ba09a8d18
ra7more <ra7more@outlook.com>
2019-05-24 15:55:48 +0800

    add user_phone_location

--- Db/tables.py
+++ Db/tables.py
@@ -6,8 +6,13 @@ from sqlalchemy import VARCHAR
 from Db.db_connect import Base
 
 
-class phone_num(Base):
+class Phone(Base):
     __tablename__ = 'Phone'
     phone_name = Column(VARCHAR(255), nullable=False, primary_key=True,default="Xiaomi-9")
     phone_num = Column(Integer, default=10000)
+
+class User(Base):
+    __tablename__ = 'User'
+    User_phone = Column(VARCHAR(255), nullable=False, primary_key=True, default='')
+    User_location = Column(VARCHAR(255), nullable=False, default=' ')
 
